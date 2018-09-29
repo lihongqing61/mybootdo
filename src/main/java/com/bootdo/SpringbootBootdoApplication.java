6f52750790a00b018c41b87a3f259588ab7984f9
lihongqing61 <lihongqing61@qq.com>
2018-09-29 09:20:50 +0800

    添加Banner

--- src/main/java/com/bootdo/SpringbootBootdoApplication.java
+++ src/main/java/com/bootdo/SpringbootBootdoApplication.java
@@ -11,5 +11,12 @@ public class SpringbootBootdoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBootdoApplication.class, args);
+		System.out.println("ヾ(◍°∇°◍)ﾉﾞ    bootdo启动成功      ヾ(◍°∇°◍)ﾉﾞ\n" +
+				" ______                    _   ______            \n" +
+				"|_   _ \\                  / |_|_   _ `.          \n" +
+				"  | |_) |   .--.    .--. `| |-' | | `. \\  .--.   \n" +
+				"  |  __'. / .'`\\ \\/ .'`\\ \\| |   | |  | |/ .'`\\ \\ \n" +
+				" _| |__) || \\__. || \\__. || |, _| |_.' /| \\__. | \n" +
+				"|_______/  '.__.'  '.__.' \\__/|______.'  '.__.'  ");
 	}
 }
